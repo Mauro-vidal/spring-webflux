4129d5b602554f80dace90ece7d1b8121c4d4d5d
Mauro-vidal <mauroviidal@gmail.com>
2025-01-28 19:50:38 -0300

    start project

---
+++ src/test/java/com/mauro/projects/webflux_course/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauro.projects.webflux_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
