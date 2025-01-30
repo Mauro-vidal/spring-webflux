e801b87531a747aca8ccbcfebb9360f3f7e5871b
Mauro-vidal <mauroviidal@gmail.com>
2025-01-30 18:02:58 -0300

    feat: adding format brazilian for LocalDatetime

--- src/main/java/com/mauro/projects/webflux_course/controller/exceptions/StandardError.java
+++ src/main/java/com/mauro/projects/webflux_course/controller/exceptions/StandardError.java
@@ -1,5 +1,6 @@
 package com.mauro.projects.webflux_course.controller.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ public class StandardError implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:s")
     private LocalDateTime timestamp;
     private String path;
     private Integer status;
