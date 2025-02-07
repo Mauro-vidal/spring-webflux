7486b62d90d5e82542407d77907ee70726828008
Mauro-vidal <mauroviidal@gmail.com>
2025-02-07 18:05:20 -0300

    feat: add unit test to handle not found user service

--- src/test/java/com/mauro/projects/webflux_course/service/UserServiceTest.java
+++ src/test/java/com/mauro/projects/webflux_course/service/UserServiceTest.java
@@ -4,6 +4,7 @@ import com.mauro.projects.webflux_course.entity.User;
 import com.mauro.projects.webflux_course.mapper.UserMapper;
 import com.mauro.projects.webflux_course.model.request.UserRequest;
 import com.mauro.projects.webflux_course.repository.UserRepository;
+import com.mauro.projects.webflux_course.service.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
@@ -14,6 +15,8 @@ import reactor.test.StepVerifier;
 
 import java.util.Objects;
 
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
@@ -115,5 +118,19 @@ class UserServiceTest {
         verify(repository, times(1)).findAndRemove(anyString());
 
 
+    }
+
+    @Test
+    void testHandleNotFound() {
+        when(repository.findById(anyString())).thenReturn(Mono.empty());
+
+        try {
+            service.findById("123").block();
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(format("Object not found. Id: %s, Type: %s", "123", User.class.getSimpleName()), ex.getMessage());
+
+
+        }
     }
 }
