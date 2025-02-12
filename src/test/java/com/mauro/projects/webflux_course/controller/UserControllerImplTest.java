1cb46d3e95ecec3491eebcf02b20a2e962302af2
Mauro-vidal <mauroviidal@gmail.com>
2025-02-12 11:30:56 -0300

    feat: add integrations test to save endpoint with bad request

--- src/test/java/com/mauro/projects/webflux_course/controller/UserControllerImplTest.java
+++ src/test/java/com/mauro/projects/webflux_course/controller/UserControllerImplTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 import static reactor.core.publisher.Mono.just;
@@ -55,6 +56,25 @@ class UserControllerImplTest {
 
         verify(service, times(1)).save(any(UserRequest.class));
 
+    }
+
+    @Test
+    @DisplayName("Test endpoint save with bad request")
+    void TestSaveWithBadRequest() {
+        final var request = new UserRequest(" Mauro", "maurovidal@mail.com", "123");
+
+        webTestClient.post().uri("/users")
+                .contentType(APPLICATION_JSON)
+                .body(fromValue(request))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/users")
+                .jsonPath("$.status").isEqualTo(BAD_REQUEST.value())
+                .jsonPath("$.error").isEqualTo("Validation Error")
+                .jsonPath("$.message").isEqualTo("Error on validation attributes")
+                .jsonPath("$.errors[0].fieldName").isEqualTo("name")
+                .jsonPath("$.errors[0].message").isEqualTo("field cannot have blank spaces at the beginning or at end");
     }
 
     @Test
